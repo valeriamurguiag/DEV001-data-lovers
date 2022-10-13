8c7884009f8e973a4d0ab94512f836c101fb9b6b
Silvia Falcon <silvi09_24@hotmail.com>
2022-10-13 09:25:33 -0500

    Avance traer data

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
+//Importar la data
 import data from './data/lol/lol.js';
 
-//Crear una cuadricula que muestre todos los personajes en pantalla
+const allChampion = data.data;
+const arrObject = Object.values(allChampion);
+// console.log(allChampion);
+// console.log(arrObject);
 
-//Mostrar data en pantalla 
-console.log(data);
+// MOSTRAR DATA
+const container = document.getElementById('container');
 
-
-
+const traerData = (arr) => {
+  arr.forEach((champion) => {
+    const newElement = document.createElement('div');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    newElement.classList.add('class-divData');
+    img.classList.add('class-img');
+    p.classList.add('class-name');
+    img.src = `${champion.splash}`;
+    p.innerHTML = `${champion.name}`;
+    newElement.appendChild(img);
+    newElement.appendChild(p);
+    container.appendChild(newElement);
+  });
+};
+traerData(arrObject);
 
